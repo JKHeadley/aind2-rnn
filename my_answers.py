ac10b561d7ffefc88c2ec8ee2533b136a282a932
Justin Headley <justin@scal.io>
2017-08-03 23:07:24 -0700

    no message

--- my_answers.py
+++ my_answers.py
@@ -37,7 +37,8 @@ def build_part1_RNN(window_size):
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-    printable = "!,.:;?0123456789abcdefghijklmnopqrstuvwxyz"
+    printable = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+                 'o','p','q','r','s','t','u','v','w','x','y','z','!', ',', '.', ':', ';', '?']
     text = ''.join(i for i in text if i in printable)
 
     return text
