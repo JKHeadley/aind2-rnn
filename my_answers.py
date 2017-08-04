b24b3b186db1d52a0975488ec1aaab156cba6e89
Justin Headley <justin@scal.io>
2017-08-04 00:02:34 -0700

    no message

--- my_answers.py
+++ my_answers.py
@@ -38,7 +38,7 @@ def build_part1_RNN(window_size):
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
     printable = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-                 'o','p','q','r','s','t','u','v','w','x','y','z','!', ',', '.', ':', ';', '?']
+                 'o','p','q','r','s','t','u','v','w','x','y','z','!',',','.',':',';','?',' ']
     text = ''.join([i for i in text if i in printable])
     # return ' '.join(i for i in text if ord(i) < 128)
 
