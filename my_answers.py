09cf7220f8f7e324e5dd894644acf8f252cc1890
Justin Headley <justin@scal.io>
2017-08-03 23:56:23 -0700

    no message

--- my_answers.py
+++ my_answers.py
@@ -39,7 +39,7 @@ def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
     printable = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
                  'o','p','q','r','s','t','u','v','w','x','y','z','!', ',', '.', ':', ';', '?']
-    text = ''.join(i for i in text if i in printable)
+    text = ''.join([i for i in text if i in printable])
 
     return text
 
